1d90ef9d9c1ce0a42dfac752b77364cf22fd8b58
FrancoFrias23 <lockke2323@gmail.com>
2023-02-14 15:35:31 -0300

    agrego rutas User

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,8 +1,48 @@
-const apiData = () => {}
+const axios= require("axios");
+const {User,Game} = require("../db.js")
 
-const dbData = () => {}
+const apiData = async() => {
+    try {
+        const data = (await axios.get(`https://jsonplaceholder.typicode.com/users`)).data;
+        let cleanData=[];
+        // const cleanData = data.map((u)=>{
+        //     user_name : u.name
+        // })
+        for (let u of data){
+            let  newUser={
+                user_name: u.name,
+                user_email: u.email,
+                user_id:u.id
+            } 
+            cleanData.push(newUser)
+        }
+        return cleanData;
+    
+    } catch (error) {
+        res.status(400).send("error")
+    }
+}
 
-const getAllUsers = () => {}
+const dbData = async() => {
+    const data= await User.findAll(
+    //     {
+    //     include:{
+    //         model:Game,
+    //         attributes:["name"],
+    //         through:{
+    //             attributes:[]
+    //         }
+    //     }   
+    // }
+    );
+    return data;
+}
+
+const getAllUsers = async() => {
+    const DatosApi= await apiData();
+    const DatosDb= await dbData();
+    return [...DatosApi,...DatosDb]
+}
 
 const createUser = () => {}
 
