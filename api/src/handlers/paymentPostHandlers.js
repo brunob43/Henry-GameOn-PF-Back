e22556dfe45e59a71c48cd10046a6a7b6bbd8072
Bruno mac <brunob43@outlook.com>
2023-02-19 17:02:08 -0300

    modifpaymentPostHandlres

--- api/src/handlers/paymentPostHandlers.js
+++ api/src/handlers/paymentPostHandlers.js
@@ -1,11 +1,16 @@
 
 
-
-
 const paymentPostHandler = async (req, res) => {
+    try {
+        const { action } = req.body
+        if(![ action ].every(Boolean)) return res.status(404).send("Falta enviar datos");
+        const newPayment= await Payment.create({game_name});
+        res.status(200).json(newPayment)
 
-     
-    return console.log(req.body);
+    } catch (error) {
+        res.status(400).json({error: error.message})
+
+    }
 
 }
 
