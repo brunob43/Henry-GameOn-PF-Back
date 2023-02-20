f869344d042d8d50320a98bca0c0154beea23bbe
Bruno mac <brunob43@outlook.com>
2023-02-20 11:27:22 -0300

    paymentStatus200

--- api/src/handlers/paymentPostHandlers.js
+++ api/src/handlers/paymentPostHandlers.js
@@ -4,8 +4,9 @@ const paymentPostHandler = async (req, res) => {
     try {
         const { id } = req.body
         if(![ id ].every(Boolean)) return res.status(404).send("Falta enviar datos");
-        const newPayment= await Payment.create({ id });
-        res.status(200).json(newPayment)
+        const newPayment= await 
+        console.log(req.body);
+        res.status(200).json(newPayment);
 
     } catch (error) {
         res.status(400).json({error: error.message})
