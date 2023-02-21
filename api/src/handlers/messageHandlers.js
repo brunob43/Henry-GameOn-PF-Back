d66304c913cb31ceaf1c6412a327b51419497f60
FrancoFrias23 <lockke2323@gmail.com>
2023-02-21 16:45:54 -0300

    add message

--- api/src/handlers/messageHandlers.js
+++ api/src/handlers/messageHandlers.js
@@ -1,4 +1,4 @@
-const { Message } = require("../models/Message")
+const { Message } = require("../db")
 // const {getAllDocs} = require("../controllers/docController.js")
 
 const getMessages = async (req,res)=>{
