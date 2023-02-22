918a256de120adffb236ff92c2252b8ca394b9c7
FrancoFrias23 <lockke2323@gmail.com>
2023-02-22 20:02:19 -0300

    gameViews

--- api/src/routes/gameRouter.js
+++ api/src/routes/gameRouter.js
@@ -1,7 +1,7 @@
 const { Router } = require("express")
 const gameRouter = Router()
 
-const {getGameHandler, getIdGameHandler, postGameHandler, updateGameHandler, deleteGameHandler} = require("../handlers/gameHandlers")
+const {getGameHandler, getIdGameHandler, postGameHandler, updateGameHandler, deleteGameHandler,gamesViewsHandler} = require("../handlers/gameHandlers")
 
 gameRouter.get("/", getGameHandler)
 
@@ -12,6 +12,8 @@ gameRouter.put("/:game_id", updateGameHandler)
 gameRouter.post("/", postGameHandler)
 
 gameRouter.delete("/:game_id", deleteGameHandler)   //Borrado lógico
+
+gameRouter.put ("/view/:game_id", gamesViewsHandler)
 
  module.exports = gameRouter
  
