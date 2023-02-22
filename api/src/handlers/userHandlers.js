fe73c3d14cfbdfde59b94ffae86e616695c5b49c
Manuel Albarracin <roxcon909@gmail.com>
2023-02-22 10:56:28 -0300

    Update userHandlers.js

--- api/src/handlers/userHandlers.js
+++ api/src/handlers/userHandlers.js
@@ -24,7 +24,8 @@ const getIDUsersHandler = async (req,res) => {
     try {
         const { id } =req.params;
         const user = await User.findOne({
-            where:{user_id:id}});
+            where:{user_id:id},
+            where:{user_deleted : false}});
         res.status(200).json(user)
     } catch (error) {
         res.status(400).send(`El id: ${id} no corresponde a un Usuario existente`)
