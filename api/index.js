b8b98b219ade2239c8d31b0f2a1ecbc42c95ce20
Bruno mac <brunob43@outlook.com>
2023-02-23 12:10:40 -0300

    index

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const port = process.env.PORT
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
     console.log(`%s listening at ${port}`);
   });
