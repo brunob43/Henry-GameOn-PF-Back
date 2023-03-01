f0a9097218e3c5d4afb3dfbb350b581441ecb208
Manuel Albarracin <roxcon909@gmail.com>
2023-03-01 16:10:11 -0300

    Update gameHandlers.js

--- api/src/handlers/gameHandlers.js
+++ api/src/handlers/gameHandlers.js
@@ -5,10 +5,12 @@ const { allGameData, allDeletedGameData } = require("../controllers/gameControll
 
 module.exports = {
     getGameHandler: async (req, res) => {
-        console.log(req.body)
-
+        
         const { name } = req.query;
         const { admin } = req.body;
+        
+        console.log(req.body)
+
 
         const allGames = await allGameData()
         const allDeletedGames = await allDeletedGameData()
