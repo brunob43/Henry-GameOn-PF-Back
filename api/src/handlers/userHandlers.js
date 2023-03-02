d828466daa58ceb4548d869d6729503dd0a658d6
Manuel Albarracin <roxcon909@gmail.com>
2023-03-02 11:52:17 -0300

    Update userHandlers.js

--- api/src/handlers/userHandlers.js
+++ api/src/handlers/userHandlers.js
@@ -113,25 +113,28 @@ const postUsersHandler = async (req,res) => {
 
 const deleteUsersHandler = async (req,res) =>{
     const { internal_id } = req.params;
-    const { deleted } = req.query
     const userToDelete = await User.findAll({where:{internal_id}})
     try {
-        console.log(userToDelete[0].user_deleted)
-        if (!deleted) {
-            await User.update({
-                user_deleted: true
-            },{
-                where:{internal_id}
-            });
-            res.status(200).json(userToDelete)
+        if (userToDelete.length) {
+            if (!userToDelete[0].user_deleted){
+                User.update({
+                    user_deleted: true
+                },{
+                    where:{internal_id}
+                })
+                res.status(200).send(userToDelete) 
+            } else {
+                User.update({
+                    user_deleted: false
+                },{
+                    where:{internal_id}
+                })
+            res.status(200).send(userToDelete) 
+            }
         } else {
-            await User.update({
-                user_deleted: false
-            },{
-                where:{internal_id}
-            });
-            res.status(200).json(userToDelete)
-        } 
+            throw Error ("El usuario no existe")
+        }
+
     } catch (error) {
         res.status(400).json({error:error.message})
     }
