5c42f58a591a7268692fbe1f08a0c4227979e273
Manuel Albarracin <roxcon909@gmail.com>
2023-03-05 22:06:04 -0300

    Update paymentPostHandlers.js

--- api/src/handlers/paymentPostHandlers.js
+++ api/src/handlers/paymentPostHandlers.js
@@ -3,28 +3,29 @@ const axios = require("axios");
 const {ACCESS_TOKEN} = process.env
 
 const paymentPostHandler = async (req, res) => {
+    const { id, data } = req.body
+
     try {
-        const { id, data } = req.body
-        if(![ id ].every(Boolean)) return res.status(404).send("Falta enviar datos");
-        const newPayment= await 
-        
-        console.log(req.body);
-        res.status(200).json(newPayment);
+        const paymentDetail = await axios.get(`https://api.mercadopago.com/v1/payments/${data.id}`, {headers: {Authorization : ACCESS_TOKEN }})
+        console.log(paymentDetail)
 
         try {
-            const paymentDetail = await axios.get(`https://api.mercadopago.com/v1/payments/${data.id}`, {headers: {Authorization : ACCESS_TOKEN }})
-            console.log(paymentDetail)
+            if(![ id ].every(Boolean)) return res.status(404).send("Falta enviar datos");
+            const newPayment= await 
+            
+            console.log(req.body);
+            res.status(200).json(newPayment);
     
-        } catch (error) {
+        }   catch (error) {
             res.status(400).json({error: error.message})
             console.log(error.message)
-        
+    
         }
 
     } catch (error) {
         res.status(400).json({error: error.message})
         console.log(error.message)
-
+    
     }
 
 }
