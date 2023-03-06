03e6f7f10cad70ccda2c77197e7b293d74576fd3
Manuel Albarracin <roxcon909@gmail.com>
2023-03-05 21:09:49 -0300

    Update PaymentController.js

--- api/src/controllers/PaymentController.js
+++ api/src/controllers/PaymentController.js
@@ -1,18 +1,17 @@
 require('dotenv').config();
-import axios from 'axios';
+const axios = require("axios");
 const {ACCESS_TOKEN} = process.env
 
-
 class PaymentController {
+  
     constructor(subscriptionService) {
       this.subscriptionService = subscriptionService;
     }
-  
     async getPaymentLink(req, res) {
       try {
         const payment = await this.subscriptionService.createPayment();
         console.log(payment)
-        const paymentDetail = axios.get(`https://api.mercadopago.com/v1/payments/${payment.data.id}`, {headers: {Authorization : ACCESS_TOKEN }})
+        const paymentDetail = await axios.get(`https://api.mercadopago.com/v1/payments/${payment.data.id}`, {headers: {Authorization : ACCESS_TOKEN }})
         console.log(paymentDetail)
         return res.json(payment);
       } catch (error) {
